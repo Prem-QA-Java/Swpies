4a07ee61cf80c6685f21b041093b789bec4e9459
prem-apalya <premsai.p@apalya.com>
2023-03-11 00:27:43 +0530

    Done

--- src/test/java/Base/runner.java
+++ src/test/java/Base/runner.java
@@ -29,7 +29,7 @@ public class runner {
 
 	@Parameters({ "employeeId", "password" })
 	@BeforeClass
-	public void d(@Optional("762") String employeeId,@Optional("prem2180%") String password) {
+	public void d(@Optional("762") String employeeId, @Optional("prem2180%") String password) {
 		WebDriverManager.chromedriver().setup();
 		d = new ChromeDriver();
 		d.get("https://apps.paybooks.in/mylogin.aspx");
@@ -44,6 +44,7 @@ public class runner {
 		Date dd = new Date();
 		SimpleDateFormat sdf1 = new SimpleDateFormat("MMMM yyyy");
 		SimpleDateFormat sdf3 = new SimpleDateFormat("HHmm");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("MM");
 		// To check if it is Sunday or Saturday if it is WH then else will execute
 		if (dd.getDay() == 0 || dd.getDay() == 6) {
 			System.exit(0);
@@ -53,42 +54,34 @@ public class runner {
 			d.findElement(By.id("txtUserName")).sendKeys(id);
 			d.findElement(By.id("txtPassword")).sendKeys(pass);
 			d.findElement(By.id("btnLogin")).click();
-			// To check the button is swiped in or swiped out if it is already swiped in ten
-			// if will execute
+			// To check the button is swiped in or swiped out if it is already swiped in then if will execute
+			Thread.sleep(10000);
 			if (d.findElement(By.xpath("//*[@ng-click='applAttendance()'][1]")).getAttribute("class")
-					.contains("newpaytimein ng-binding ng-hide")) {
+					.equals("newpaytimein ng-binding ng-hide")) {  //LOgout code
 				String lastSwiped = d.findElement(By.xpath("//*[@ng-if='lastSwipedStatus']//p")).getText();
 				String[] lastSwipedSplited = lastSwiped.split("-");
 				String time2 = lastSwipedSplited[1].replace(" ", "").replace(":", "");
 				int time3 = Integer.parseInt(time2);
 				time = time3 + 900;
-				System.out.println("Login time: " + time3);
-				System.out.println("Added 9 hours to login time: " + time);
 				int presentTime = Integer.parseInt(sdf3.format(dd));
-				System.out.println("Present time: "+presentTime);
-				// This will check if the 9h are completed are not if 9h are completed then if
-				// will execute
+				// This will check if the 9h are completed are not if 9h are completed then if will execute
 				if (time <= presentTime) {
-						d.findElement(By.xpath("//*[@ng-click='applAttendance()'][2]")).click();
+					d.findElement(By.xpath("//*[@ng-click='applAttendance()'][2]")).click();
 				} else {
 					Random randomGenerator = new Random();
 					int num = randomGenerator.nextInt(60) + 1;
 					long pause = (long) ((time - presentTime) + num);
-					System.out.println("Random genarated number: " + num);
-					System.out.println("Time to pause: " + pause);
-					TimeUnit.HOURS.sleep(pause);
+					Thread.sleep((pause * 60 / 100) * 60000);
 					d.findElement(By.xpath("//*[@ng-click='applAttendance()'][2]")).click();
 					d.findElement(By.id("menu1")).click();
 					d.findElement(By.xpath("//*[@id='topNavigation']//a[@ng-click='logout()']//span")).click();
 				}
-			} else {
+			} else {      //Login code
 				d.findElement(By.xpath(
 						"//*[@id='abc1']/div/div/div/div[1]/div/div[1]/div[2]/div[3]/div/div/div[2]/table/tbody/tr/td[1]"))
 						.click();
 				d.findElement(By.xpath("//*[text()='Calendar View']")).click();
 				String mouth = d.findElement(By.xpath("//*[@id='calendar']//h2")).getText();
-				System.out.println(mouth);
-				System.out.println(sdf1.format(dd));
 				if (mouth.equals(sdf1.format(dd))) {
 					try {
 						d.findElement(By.xpath("//*[@class='fc-content']//*[text()='A']"));
@@ -97,24 +90,60 @@ public class runner {
 						present = false;
 					}
 				} else {
-					try {
-						d.findElement(By.xpath("//*[@class='fc-right']/button")).click();
-						if (mouth.equals(sdf1.format(dd))) {
-							present = true;
+					String calender = d.findElement(By.xpath("//*[@id='calendar']//div[3]/div[1]//td[4]"))
+							.getAttribute("data-date");
+					String[] calenderSplit = calender.split("-");
+					int calenderMounth = Integer.parseInt(calenderSplit[1]);
+					int presentMonth = Integer.parseInt(sdf2.format(dd));
+					while (true) {
+						if (presentMonth > calenderMounth) {
+							d.findElement(By.xpath("//*[@class='fc-right']/button")).click();
+							calender = d.findElement(By.xpath("//*[@id='calendar']//div[3]/div[1]//td[4]"))
+									.getAttribute("data-date");
+							calenderSplit = calender.split("-");
+							calenderMounth = Integer.parseInt(calenderSplit[1]);
+							if (presentMonth == calenderMounth) {
+								try {
+									d.findElement(By.xpath("//*[@class='fc-content']//*[text()='A']"));
+									present = true;
+									break;
+								} catch (NoSuchElementException e) {
+									break;
+								}
+							}
+						} else if (presentMonth < calenderMounth) {
+							d.findElement(By.xpath("//*[@class='fc-left']/button")).click();
+							calender = d.findElement(By.xpath("//*[@id='calendar']//div[3]/div[1]//td[4]"))
+									.getAttribute("data-date");
+							calenderSplit = calender.split("-");
+							calenderMounth = Integer.parseInt(calenderSplit[1]);
+							if (presentMonth == calenderMounth) {
+								try {
+									d.findElement(By.xpath("//*[@class='fc-content']//*[text()='A']"));
+									present = true;
+									break;
+								} catch (NoSuchElementException e) {
+									break;
+								}
+							}
+						} else {
+							try {
+								d.findElement(By.xpath("//*[@class='fc-content']//*[text()='A']"));
+								present = true;
+								break;
+							} catch (NoSuchElementException e) {
+								break;
+							}
 						}
-					} catch (ElementClickInterceptedException e) {
-						d.findElement(By.xpath("//*[@class='fc-left']/button")).click();
+
 					}
 				}
 				if (present == true) {
 					d.findElement(By.xpath("//*[@ng-click='switchversion()']")).click();
 					Random randomGenerator = new Random();
 					int time = randomGenerator.nextInt(60) + 1;
-					time = time * 1000;
-					System.out.println(time);
+					time = time * 60000;
 					Thread.sleep(time);
-					int presentTime = Integer.parseInt(sdf3.format(dd));
-					System.out.println("present" + presentTime);
 					d.findElement(By.xpath("//*[@ng-click='applAttendance()'][1]")).click();
 					d.findElement(By.id("menu1")).click();
 					d.findElement(By.xpath("//*[@id='topNavigation']//a[@ng-click='logout()']//span")).click();
@@ -126,6 +155,6 @@ public class runner {
 
 	@AfterClass
 	public void exit() {
-		d.close();
+		d.quit();
 	}
 }
